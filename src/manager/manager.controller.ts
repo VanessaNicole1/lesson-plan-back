6d76148e2f0dfa81bf3b565bfe849dce7f8533e0
AlexisC <alexisc@pop-os.localdomain>
2022-08-10 23:25:28 -0500

    Update manager entity

--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { ManagerService } from './manager.service';
 import { Manager } from './manager.entity';
 import { Body } from '@nestjs/common';
@@ -18,8 +18,16 @@ export class ManagerController {
     return this.managerService.createManager(createManagerDto);
   }
 
-  @Patch()
-  updateManager(updateManagerDto: CreateorUpdateManagerDto) {
-    return this.managerService.updateManager(updateManagerDto);
+  @Delete('/:id')
+  deleteManager(@Param('id') id: string): Promise<void> {
+    return this.managerService.deleteManager(id);
+  }
+
+  @Put(':id')
+  updateManager(
+    @Param('id') id: string,
+    @Body() updateManagerDto: CreateorUpdateManagerDto,
+  ) {
+    return this.managerService.updateManager(id, updateManagerDto);
   }
 }
