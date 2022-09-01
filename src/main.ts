d63ea379cb5acbfca09519d4beb98be210aab818
Vanessa <vanessainiguez98@gmail.com>
2022-08-31 20:48:07 -0500

    feat: Add cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(3000);
-  app.enableCors();
 }
 bootstrap();
