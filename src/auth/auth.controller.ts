c0892c39f847be2efbff7d252894731b34737aeb
Vanessa <vanessainiguez98@gmail.com>
2022-11-26 14:32:52 -0500

    feat: Create a refresh token to authetication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login-dto';
+import { RefreshTokenGuard } from './jwt-refresh-token.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +20,7 @@ export class AuthController {
   @Post()
   async login(
     @Body() authLoginDto: LoginDTO,
-  ): Promise<{ access_token: string }> {
+  ): Promise<{ accessToken: string }> {
     const { email, password } = authLoginDto;
     const isUserValid = await this.authService.validateUser(email, password);
     if (!isUserValid) {
@@ -18,5 +28,19 @@ export class AuthController {
     }
 
     return await this.authService.login(authLoginDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('logout')
+  async logout(@Request() req: any) {
+    return await this.authService.logout(req.user.id);
+  }
+
+  @UseGuards(RefreshTokenGuard)
+  @Get('refresh')
+  async refreshTokens(@Request() req: any) {
+    const userId = req.user.id;
+    const refreshToken = req.user.refreshToken;
+    return await this.authService.refreshTokens(userId, refreshToken);
   }
 }
---
+++ src/auth/refreshToken.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JWTPayload } from './jwt.payload';
+import { UserService } from 'src/user/users.service';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private usersService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_REFRESH_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: JWTPayload) {
+    const user = await this.usersService.getUserById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+    return { ...user, refreshToken };
+  }
+}
