0a49f0239e7184e1f9a0389757c9375bfb157eb4
Vanessa <vanessainiguez98@gmail.com>
2022-12-01 23:29:08 -0500

    feat: Validate the repeated emails

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,4 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { generate } from 'generate-password';
 import { extname } from 'path';
 export class Helpers {
@@ -17,10 +18,38 @@ export class Helpers {
     const plainPassword = generate({
       length: 10,
       numbers: true,
-      symbols: true,
       lowercase: true,
       uppercase: true,
     });
     return plainPassword;
+  }
+
+  static validateCsv(data: any, columns: any) {
+    const rows = data.split(/\r?\n/).filter((row) => /\S/.test(row));
+    const header = rows[0].replace(/\s+/g, '');
+    const currentData = rows.slice(1);
+    if (header != columns.join(',')) {
+      throw new Error(
+        `El archivo CSV es inválido, debe ser de esta manera ${columns.join(
+          ',',
+        )}`,
+      );
+    }
+    return currentData.map((row) => {
+      const newRow = {};
+      let items = row.replace(/\s+/g, '');
+      items = items.split(',');
+
+      if (items.length < columns.length - 1) {
+        throw new Error(
+          `Por favor cambiar el formato, cada columna debe contener ${
+            columns.length - 1
+          } elementos`,
+        );
+      }
+
+      columns.forEach((name, index) => (newRow[name] = items[index]));
+      return newRow;
+    });
   }
 }
