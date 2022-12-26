c4fcca8beb0e8af07f4bddf560824ae474295cf9
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 01:00:15 -0500

    feat: Update Degree Entity

--- src/modules/degree/degree.entity.ts
+++ src/modules/degree/degree.entity.ts
@@ -1,11 +1,14 @@
 import {
   Column,
   Entity,
+  JoinColumn,
   ManyToOne,
   OneToMany,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Grade } from '../grade/grade.entity';
+import { Manager } from '../manager/manager.entity';
 import { Period } from '../period/period.entity';
 
 @Entity()
@@ -15,6 +18,10 @@ export class Degree {
 
   @Column()
   name: string;
+
+  @OneToOne(() => Manager)
+  @JoinColumn({ name: 'manager_id' })
+  manager: Manager;
 
   @ManyToOne(() => Period, (period) => period.degrees)
   period: Period;
