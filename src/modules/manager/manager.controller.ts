86431d7caec9cce788d2125d815eda81441dc475
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 01:02:09 -0500

    feat: Update Manager Entity

--- src/modules/manager/manager.controller.ts
+++ src/modules/manager/manager.controller.ts
@@ -12,20 +12,13 @@ import { Manager } from './manager.entity';
 import { Body } from '@nestjs/common';
 import { CreateorUpdateManagerDto } from './dto/create-update-manager.dto';
 import { AuthGuard } from '@nestjs/passport';
-import { ValidManager } from 'src/modules/auth/valid-manager.guard';
 import { Roles } from 'src/modules/auth/roles.decorator';
 import { Role } from 'src/modules/auth/enums/role.enum';
+import { ValidManager } from '../auth/guards/valid-manager.guard';
 
 @Controller('manager')
 export class ManagerController {
   constructor(private managerService: ManagerService) {}
-
-  @UseGuards(AuthGuard('jwt'), ValidManager)
-  @Roles(Role.Manager)
-  @Get('/:id')
-  getManagerById(@Param('id') id: string): Promise<Manager> {
-    return this.managerService.getManagerById(id);
-  }
 
   @Post()
   createManager(@Body() createManagerDto: CreateorUpdateManagerDto) {
@@ -34,14 +27,28 @@ export class ManagerController {
 
   @UseGuards(AuthGuard('jwt'), ValidManager)
   @Roles(Role.Manager)
+  @Get('/:id')
+  getManagerById(@Param('id') id: string): Promise<Manager> {
+    return this.managerService.getManagerById(id);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'), ValidManager)
+  @Roles(Role.Manager)
+  getAllManager() {
+    return this.managerService.getManagers();
+  }
+
+  @UseGuards(AuthGuard('jwt'), ValidManager)
+  @Roles(Role.Manager)
   @Delete('/:id')
-  deleteManager(@Param('id') id: string): Promise<void> {
+  deleteManager(@Param('id') id: string) {
     return this.managerService.deleteManager(id);
   }
 
   @UseGuards(AuthGuard('jwt'), ValidManager)
   @Roles(Role.Manager)
-  @Put(':id')
+  @Put('/:id')
   updateManager(
     @Param('id') id: string,
     @Body() updateManagerDto: CreateorUpdateManagerDto,
