5fca088814afc11ef4b02f2f3c78138639e25b04
VanessaNicole1 <nicolegualan@gmail.com>
2023-01-28 15:14:24 -0500

    feat: Create teacher with subject and grade dto

---
+++ src/modules/teachers/dto/create-teacher-subject-grade-dto.ts
@@ -0,0 +1,9 @@
+export class CreateTeacherSubjectGradeDto {
+  name: string;
+  lastName: string;
+  email: string;
+  numberParallel: string;
+  parallel: string;
+  subject: string;
+  password?: string;
+}
