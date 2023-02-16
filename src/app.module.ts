f1a60434c58dd6b58ca425cc54db4096c4a4ca43
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-15 22:34:47 -0500

    Update app.module.ts
    Add Common module to the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { CommonModule } from './modules/common/common.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
