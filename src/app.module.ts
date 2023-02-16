c99454d2c2df0cc7117d8f8e133b2f9073f54dc2
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-16 15:59:30 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { CommonModule } from './modules/common/common.module';
+import { PeriodsModule } from './modules/periods/periods.module';
+import { DegreesModule } from './modules/degrees/degrees.module';
+import { GradesModule } from './modules/grades/grades.module';
+import { InitialProcessModule } from './modules/initial-process/initial-process.module';
+import { LessonPlansModule } from './modules/lesson-plans/lesson-plans.module';
+import { ManagersModule } from './modules/managers/managers.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { SchedulesModule } from './modules/schedules/schedules.module';
+import { StudentsModule } from './modules/students/students.module';
+import { SubjectsModule } from './modules/subjects/subjects.module';
+import { TeachersModule } from './modules/teachers/teachers.module';
 
 @Module({
-  imports: [CommonModule, UsersModule],
+  imports: [
+    CommonModule,
+    UsersModule,
+    PeriodsModule,
+    DegreesModule,
+    GradesModule,
+    InitialProcessModule,
+    LessonPlansModule,
+    ManagersModule,
+    RolesModule,
+    SchedulesModule,
+    StudentsModule,
+    SubjectsModule,
+    TeachersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
