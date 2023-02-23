c19f5dd10b5c64f785c98ddd2129dbab0a7fcb6c
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-23 15:07:42 -0500

    Create email.utils.ts
    Add utilities functions for Emails.

---
+++ src/utils/email.utils.ts
@@ -0,0 +1,16 @@
+export const isEmailDomainValid = (email: string, domain: string = '@unl.edu.ec'): boolean => email.includes(domain);
+
+export const getDuplicatedEmails = (emails: string[]) => {
+  const uniqueEmails = [];
+  const duplicatedEmails = [];
+
+  for (const email of emails) {
+    if (uniqueEmails.includes(email)) {
+      duplicatedEmails.push(email);
+    } else {
+      uniqueEmails.push(email);
+    }
+  }
+
+  return duplicatedEmails;
+};
