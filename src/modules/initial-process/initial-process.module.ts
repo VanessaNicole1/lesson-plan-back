dbc9f831de999a82e01ea3dd8ab29e9828b52ace
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-23 15:11:45 -0500

    Update initial-process.module.ts
    Import the necessary module to be used inside initial process module.

--- src/modules/initial-process/initial-process.module.ts
+++ src/modules/initial-process/initial-process.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { InitialProcessService } from './initial-process.service';
 import { InitialProcessController } from './initial-process.controller';
 import { InitialProcessRepository } from './initial-process.repository';
+import { PeriodsModule } from '../periods/periods.module';
+import { UsersModule } from '../users/users.module';
+import { StudentsModule } from '../students/students.module';
+import { TeachersModule } from '../teachers/teachers.module';
+import { GradesModule } from '../grades/grades.module';
 
 @Module({
+  imports: [
+    PeriodsModule,
+    UsersModule,
+    StudentsModule,
+    TeachersModule,
+    GradesModule
+  ],
   controllers: [InitialProcessController],
   providers: [InitialProcessService, InitialProcessRepository]
 })
