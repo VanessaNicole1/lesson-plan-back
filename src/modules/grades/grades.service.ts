c01c20d077e4b82176f9463ba7e8ae9f7fcc0ef0
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-23 15:08:53 -0500

    Update grades.service.ts
    Add function to validate the match between students and teachers content.

--- src/modules/grades/grades.service.ts
+++ src/modules/grades/grades.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateGradeDto } from './dto/create-grade.dto';
 import { UpdateGradeDto } from './dto/update-grade.dto';
+import { ValidateGradesMatchDto } from './dto/validate-grades-match.dto';
 import { GradesRepository } from './grades.repository';
 
 @Injectable()
@@ -26,5 +27,28 @@ export class GradesService {
 
   remove(id: number) {
     return `This action removes a #${id} grade`;
+  }
+
+  validateGradesMatch(validateGradesMatchDto: ValidateGradesMatchDto) {
+    const { students, teachers } = validateGradesMatchDto;
+
+    const getGrades = ({ numberParallel, parallel }) => `${numberParallel} ${parallel}`;
+
+    const studentsGrades = [... new Set(students.map(getGrades))];
+    const teachersGrades = [... new Set(teachers.map(getGrades))];
+    const notMatchingGrades = [];
+
+    for (const studentGrade of studentsGrades) {
+      const matchingGradeIndex = teachersGrades.indexOf(studentGrade);
+    
+      if (matchingGradeIndex === -1) {
+        notMatchingGrades.push(studentGrade);
+      }
+    }
+
+    if (notMatchingGrades.length > 0) {
+      const message = 'En el archivo de estudiantes, los siguientes cursos no constan en la lista de docentes:';
+      throw new BadRequestException(`${message}: ${notMatchingGrades.join(', ')}`);
+    };
   }
 }
