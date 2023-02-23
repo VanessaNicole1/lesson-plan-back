7cf06964132782d2e260cf55a81ddcbf41e98f9d
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-23 15:09:49 -0500

    Update create-user.dto.ts

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  name: string
+  
+  @IsString()
+  lastName: string
+
+  @IsEmail()
+  email: string
+
+  @IsString()
+  password: string
+}
