3cd2a8e05b0e51e19fe065f59ce711efdde1ec10
VanessaNicole1 <nicolegualan@gmail.com>
2023-02-26 15:38:15 -0500

    feat: Add UseGuard to create initial process

--- src/modules/initial-process/initial-process.controller.ts
+++ src/modules/initial-process/initial-process.controller.ts
@@ -6,16 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { InitialProcessService } from './initial-process.service';
 import { CreateInitialProcessDto } from './dto/create-initial-process.dto';
 import { UpdateInitialProcessDto } from './dto/update-initial-process.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ValidManager } from '../../utils/guards/valid-manager.guard';
+import { Roles } from '../../utils/decorators/roles.decorator';
+import { Role } from '../../utils/enums/roles.enum';
 
 @Controller('initial-process')
 export class InitialProcessController {
   constructor(private readonly initialProcessService: InitialProcessService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, ValidManager)
+  @Roles(Role.Manager)
   create(@Body() createInitialProcessDto: CreateInitialProcessDto) {
     return this.initialProcessService.create(createInitialProcessDto);
   }
