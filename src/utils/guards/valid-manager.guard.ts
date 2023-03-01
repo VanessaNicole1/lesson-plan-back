1a8b2ad2d30586389592b69b46fc57032fde2efe
VanessaNicole1 <nicolegualan@gmail.com>
2023-03-01 00:01:49 -0500

    fix: Remove unnecessary logic

--- src/utils/guards/valid-manager.guard.ts
+++ src/utils/guards/valid-manager.guard.ts
@@ -1,28 +1,23 @@
-import { User } from '../../modules/users/entities/user.entity';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { roles } from './../variables.utils';
+import { User } from '../../modules/users/entities/user.entity';
 import { Role } from '../enums/roles.enum';
 
 @Injectable()
 export class ValidManager implements CanActivate {
   constructor(private reflector: Reflector) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const rolesKey = 'roles';
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(rolesKey, [
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(roles, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    console.log('REQ requiredRoles', requiredRoles);
-
-    for (let i = 0; i < requiredRoles.length; i++) {
-      console.log('requiredRoles', requiredRoles[i]);
-    }
-
     const request = context.switchToHttp().getRequest();
     const currentUser: User = request.user;
-    const roles = currentUser.roles;
-    const rolesType = roles.map((role) => role.name);
+    const userRoles = currentUser.roles;
+    const rolesType = userRoles.map((role) => role.name);
 
     if (requiredRoles.includes(Role.Manager)) {
       if (rolesType.includes(requiredRoles[0])) {
