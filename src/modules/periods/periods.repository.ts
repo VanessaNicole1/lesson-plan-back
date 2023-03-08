5d65667106159bdf8b0e69d9a2afec5f0657f8f4
VanessaNicole1 <nicolegualan@gmail.com>
2023-03-07 20:22:14 -0500

    feat: Find all active periods

--- src/modules/periods/periods.repository.ts
+++ src/modules/periods/periods.repository.ts
@@ -1,11 +1,17 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../common/services/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/services/prisma.service';
+import { FilterPeriodDto } from './dto/filter-period.dto';
 
 @Injectable()
 export class PeriodsRepository {
   constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return this.prisma.period.findMany();
+  findAll(filterPeriodDto: FilterPeriodDto = {}) {
+    const { isActive } = filterPeriodDto;
+    return this.prisma.period.findMany({
+      where: {
+        isActive,
+      },
+    });
   }
 }
