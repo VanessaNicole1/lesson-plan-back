02ac42299f172678afad43663814abd985ea0d75
VanessaNicole1 <nicolegualan@gmail.com>
2023-03-07 20:21:06 -0500

    feat: Find all manager by period

--- src/modules/managers/managers.repository.ts
+++ src/modules/managers/managers.repository.ts
@@ -1,10 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/services/prisma.service';
+import { Role } from '../../utils/enums/roles.enum';
 @Injectable()
 export class ManagersRepository {
   constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return this.prisma.manager.findMany();
+  findAll(id: string) {
+    if (id) {
+      return this.prisma.manager.findMany({
+        where: {
+          degree: {
+            period: {
+              id,
+            },
+          },
+        },
+        include: {
+          user: {
+            include: {
+              roles: true,
+            },
+          },
+        },
+      });
+    }
+    return this.prisma.manager.findMany({
+      include: {
+        user: {
+          include: {
+            roles: {
+              where: {
+                name: Role.Teacher,
+              },
+            },
+          },
+        },
+      },
+    });
   }
 }
