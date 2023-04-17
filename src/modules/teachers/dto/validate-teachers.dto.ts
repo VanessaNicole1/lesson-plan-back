9f341d1b153c1a11815288cc321f21523c0ad641
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-17 13:16:41 -0500

    Use i18nValidation message function to allow i18n inside DTOs validation.

--- src/modules/teachers/dto/validate-teachers.dto.ts
+++ src/modules/teachers/dto/validate-teachers.dto.ts
@@ -1,11 +1,13 @@
 import { Type } from 'class-transformer';
 import { ArrayNotEmpty, IsArray, ValidateNested } from 'class-validator';
 import { CreateTeacherDto } from './create-teacher.dto';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
+const baseI18nKey = 'teachers.dtos.validateTeachersDto';
 export class ValidateTeachersDto {
   @IsArray()
   @ArrayNotEmpty({
-    message: 'Debe existir al menos un docente para validar',
+    message: i18nValidationMessage(`${baseI18nKey}.EMPTY_ARRAY`),
   })
   @ValidateNested({ each: true })
   @Type(() => CreateTeacherDto)
