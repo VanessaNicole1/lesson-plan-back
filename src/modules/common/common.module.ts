42afd164e1d744bbd0325d2ba7d2888ec401052d
VanessaNicole1 <nicolegualan@gmail.com>
2023-05-15 00:05:51 -0500

    Update common.module.ts
    Add fake email service.

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma.service';
 import { SendEmailService } from './services/send-email.service';
+import { SendFakeEmailService } from './services/send-fake-email.service';
 
 @Global()
 @Module({
-  providers: [PrismaService, SendEmailService],
-  exports: [PrismaService, SendEmailService]
+  providers: [PrismaService, SendEmailService, SendFakeEmailService],
+  exports: [PrismaService, SendEmailService, SendFakeEmailService]
 })
 export class CommonModule {}
