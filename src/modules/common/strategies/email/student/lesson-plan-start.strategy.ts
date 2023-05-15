87b485e307d0d3ac665871ecb1553d312ba41215
VanessaNicole1 <nicolegualan@gmail.com>
2023-05-15 00:07:11 -0500

    Add Email Lesson Plan Start strategy for student

---
+++ src/modules/common/strategies/email/student/lesson-plan-start.strategy.ts
@@ -0,0 +1,35 @@
+import { EmailStrategy } from '../../../interfaces/email/email-strategy.interface';
+import { getEmailTemplate } from '../../../../../utils/template.utils';
+
+export class StudentLessonPlanStartEmail implements EmailStrategy {
+  constructor(
+    private readonly studentName: string,
+    private readonly lessonPlanYears: { startYear: number, endYear: number }, 
+    private readonly registerToken: string | undefined = undefined,
+  ) {}
+
+  getData() {
+    const { startYear, endYear } = this.lessonPlanYears;
+    const data: any = {
+      name: this.studentName,
+      startYear,
+      endYear
+    };
+
+    if (this.registerToken) {
+      data.url = `${process.env.FRONTEND}/auth/register/${this.registerToken}`;
+    } else {
+      data.url = `${process.env.FRONTEND}/auth/login`
+    }
+
+    return data;
+  }
+
+  getTemplate(data: any) {
+    return getEmailTemplate('templates/student/lesson-plan-start.html', data);
+  }
+
+  getSubject(): string {
+    return 'Estudiante - Inicio Plan de Clases';
+  }
+}
