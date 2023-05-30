b8d131f8825bc8d092ef1e3683191bdacdf0e864
VanessaNicole1 <nicolegualan@gmail.com>
2023-05-30 00:02:48 -0500

    feat: Create validate students number method

--- src/modules/number-students-graded/dto/create-minimum-number-students-to-evaluate.dto.ts
+++ src/modules/number-students-graded/dto/create-minimum-number-students-to-evaluate.dto.ts
@@ -1,7 +1,10 @@
-import { IsInt, IsNotEmpty } from "class-validator";
+import { IsDefined, IsInt, IsNotEmpty } from "class-validator";
+import { i18nValidationMessage } from 'nestjs-i18n';
 
+const baseI18nKey = 'initial-process.dtos.createInitialProcessDto';
 export class CreateMinimumNumberOfStudentsToEvaluateDto  {
     @IsNotEmpty()
     @IsInt()
+    @IsDefined({ message: i18nValidationMessage(`${baseI18nKey}.NUMBER_STUDENTS_REQUIRED`) })
     minimumStudentsToEvaluate: number;
 }
--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -8,10 +8,13 @@ import { FilterStudentDto } from './dto/filter-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { StudentsRepository } from './students.repository';
 import { I18nContext } from 'nestjs-i18n';
+import { ValidateStudentsNumberDto } from './dto/validate-students-number.dto';
 
 @Injectable()
 export class StudentsService {
   readonly baseI18nKey = 'students.service';
+
+  readonly initialProcessBaseI18nKey = 'initial-process.repository';
   
   constructor(private studentsRepository: StudentsRepository) {}
 
@@ -56,6 +59,32 @@ export class StudentsService {
 
     for (const studentDto of createStudentsDto) {
       this.validateStudentEmail(studentDto, i18nContext);
+    }
+  }
+
+  validateStudentsNumber(validateStudentsNumberDto: ValidateStudentsNumberDto, minimumStudents: number, i18nContext: I18nContext) {
+    let { students } = validateStudentsNumberDto;
+    let mismatchedGrades = [];
+
+    for (let i = 0; i < students.length; i++) {
+      const student = students[i];
+      let count = 0;
+      const keyToValidate = JSON.stringify({ numberParallel: student.numberParallel, parallel: student.parallel });
+      for (let j = 0; j < students.length; j++) {
+        const studentToValidate = students[j];
+        const keyValue = JSON.stringify({ numberParallel: studentToValidate.numberParallel, parallel: studentToValidate.parallel });
+        if (keyToValidate === keyValue) {
+          count += 1;
+        }
+      }
+      if (count < minimumStudents) {
+        mismatchedGrades.push(`${student.numberParallel} "${student.parallel}" `);
+      }
+      students = students.filter(obj => JSON.stringify({ numberParallel: obj.numberParallel, parallel: obj.parallel }) !== JSON.stringify({ numberParallel: student.numberParallel, parallel: student.parallel }) );
+    }
+    
+    if (mismatchedGrades.length > 0) {
+      throw new BadRequestException(`${i18nContext.t(`${this.initialProcessBaseI18nKey}.create.MINIMUM_STUDENTS`)} ${mismatchedGrades.join(', ')}`);
     }
   }
 }
