7b0b14dc18c964b4217ec5cc63e0e2219a882ee9
VanessaNicole1 <nicolegualan@gmail.com>
2023-06-05 17:52:36 -0500

    feat: Update Lesson Plan Module

--- src/modules/lesson-plans/lesson-plans.module.ts
+++ src/modules/lesson-plans/lesson-plans.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { LessonPlansService } from './lesson-plans.service';
 import { LessonPlansController } from './lesson-plans.controller';
 import { LessonPlansRepository } from './lesson-plans.repository';
+import { SchedulesModule } from '../schedules/schedules.module';
 
 @Module({
+  imports: [
+    SchedulesModule
+  ],
   controllers: [LessonPlansController],
   providers: [LessonPlansService, LessonPlansRepository]
 })
--- src/modules/lesson-plans/lesson-plans.repository.ts
+++ src/modules/lesson-plans/lesson-plans.repository.ts
@@ -1,11 +1,83 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../common/services/prisma.service";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../common/services/prisma.service';
+import { CreateLessonPlanDto } from './dto/create-lesson-plan.dto';
+import { ValidateLessonPlanDto } from './dto/validate-lesson-plan.dto';
+import { UpdateLessonPlanDto } from './dto/update-lesson-plan.dto';
 
 @Injectable()
 export class LessonPlansRepository {
   constructor(private prisma: PrismaService) {}
 
+  private getAdittionalData() {
+    return {
+      include: {
+        validationsTracking: true,
+        schedule: true,
+      },
+    };
+  }
+
   findAll() {
     return this.prisma.lessonPlan.findMany();
+  }
+
+  async findOne(id: string) {
+    const lessonPlan = await this.prisma.lessonPlan.findUnique({
+      where: {
+        id,
+      },
+      ...this.getAdittionalData(),
+    });
+
+    if (!lessonPlan) {
+      throw new NotFoundException(`Plan de clases con id "${id}" no encontrado`);
+    }
+
+    return lessonPlan;
+  }
+
+  findLessonPlanBySchedule(scheduleId: string) {
+    return this.prisma.lessonPlan.findMany({
+      where: {
+        scheduleId,
+      },
+      ...this.getAdittionalData()
+    })
+  }
+
+  create(createLessonPlanDto: CreateLessonPlanDto) {
+    const { date, topic, content, scheduleId, resources } = createLessonPlanDto;
+    return this.prisma.lessonPlan.create({
+      data: {
+        date: new Date(date),
+        topic,
+        content,
+        scheduleId,
+      }
+    })
+  }
+
+  async update(id: string, updateLessonPlanDto: UpdateLessonPlanDto) {
+    await this.findOne(id);
+    const updatedLessonPlan = await this.prisma.lessonPlan.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateLessonPlanDto,
+        date: updateLessonPlanDto.date && new Date(updateLessonPlanDto.date)
+      },
+      ...this.getAdittionalData(),
+    });
+    return updatedLessonPlan;
+  }
+
+  async remove(id: string) {
+    const deleteLessonPlan = await this.prisma.lessonPlan.delete({
+      where: {
+        id,
+      },
+    });
+    return deleteLessonPlan;
   }
 }
