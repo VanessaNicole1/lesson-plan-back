93447b49bab0205b814cb9c05cb34e0f7a25069e
VanessaNicole1 <nicolegualan@gmail.com>
2023-06-19 11:58:15 -0500

    feat: Create Lesson Plan Tracking

--- src/modules/lesson-plan-validation-tracking/lesson-plan-tracking.controller.ts
+++ src/modules/lesson-plan-validation-tracking/lesson-plan-tracking.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { LessonPlansTrackingService } from './lesson-plan-tracking.service';
+import { CreateLessonPlanTrackingDto } from './dto/validate-lesson-plan.dto';
 
 @Controller('lesson-plans-tracking')
 export class LessonPlansTrackingController {
-  constructor(private readonly lessonPlansTrackingService: LessonPlansTrackingService) {}
+  constructor(
+    private readonly lessonPlansTrackingService: LessonPlansTrackingService,
+  ) {}
+
+  @Post()
+  create(@Body() createLessonPlanTrackingDto: CreateLessonPlanTrackingDto) {
+    return this.lessonPlansTrackingService.create(createLessonPlanTrackingDto);
+  }
 }
