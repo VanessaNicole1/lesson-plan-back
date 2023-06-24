a7b83e28fefa5fd33eda5408a00f213ac8b5a6d3
VanessaNicole1 <nicolegualan@gmail.com>
2023-06-24 16:29:17 -0500

    feat: Update schedule schema

---
+++ prisma/migrations/20230624212015_update_schedule_model/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `day` on the `Schedule` table. All the data in the column will be lost.
+  - You are about to drop the column `endHour` on the `Schedule` table. All the data in the column will be lost.
+  - You are about to drop the column `startHour` on the `Schedule` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Schedule" DROP COLUMN "day",
+DROP COLUMN "endHour",
+DROP COLUMN "startHour";
