993bfb4d08a417e612c8990b61f73f32946e8d94
Vanessa Iñiguez <vanessainiguez98@gmail.com>
2023-08-15 18:09:34 -0500

    fix: Fix role assignment

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -2,9 +2,10 @@ import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['email'] as const),
+  OmitType(CreateUserDto, ['email', 'roleIds'] as const),
 ) {
   phoneNumber: number;
   city: string;
   identificationCard: string;
+  roles: any[];
 }
