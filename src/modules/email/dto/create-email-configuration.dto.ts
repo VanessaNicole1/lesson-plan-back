b9c8490986cb00f85d37157bf529e9288505e022
Vanessa Iñiguez <vanessainiguez98@gmail.com>
2023-09-30 15:41:25 -0500

    feat: Execute migration and add find method

--- src/modules/email/dto/create-email-configuration.dto.ts
+++ src/modules/email/dto/create-email-configuration.dto.ts
@@ -1,8 +1,8 @@
 import { IsNumber, IsString } from "class-validator";
 
 export class CreateEmailConfigurationDto {
-  @IsNumber()
-  host: number;
+  @IsString()
+  host: string;
 
   @IsNumber()
   port: number;
